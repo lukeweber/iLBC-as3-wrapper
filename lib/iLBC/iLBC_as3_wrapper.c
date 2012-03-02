d62221d30d322c0744fd462875382e0e9c076ee4
Luke Weber <luke@tuenti.com>
2012-03-02 11:10:06 +0100

    Fixing some bugs, hopefully we're async now

--- lib/iLBC/iLBC_as3_wrapper.c
+++ lib/iLBC/iLBC_as3_wrapper.c
@@ -113,70 +113,63 @@ static void encodeForFlash(void * self, AS3_Val args)
 {
 	AS3_Val open, progress, complete;
 	AS3_Val src, dest;
-	int len;
+	int len, srcLen, yieldTicks;
 	short raw_data[BLOCKL_MAX], encoded_data[ILBCNOOFWORDS_MAX];
 
-	AS3_ArrayValue(args, "AS3ValType, AS3ValType, AS3ValType, AS3ValType, AS3ValType", &open, &progress, &complete, &src, &dest);
+	AS3_ArrayValue(args, "AS3ValType, AS3ValType, AS3ValType, IntType, IntType", &progress, &src, &dest, &srcLen, &yieldTicks);
 
-	AS3_Call(open, NULL, NULL);
-	
 	iLBC_Enc_Inst_t Enc_Inst;
 	initEncode(&Enc_Inst, 30);
-    int i = 0;
-	while(AS3_ByteArray_readBytes(&raw_data, src, (Enc_Inst.blockl * sizeof(short)))){
-        len = encode(&Enc_Inst, encoded_data, raw_data);
-		AS3_ByteArray_writeBytes(dest, &encoded_data, len);
-		if(i++ % 10 == 0){
+	int i;
+	int loops = srcLen / sizeof(short) / Enc_Inst.blockl;
+	for(i = 0; i < loops; i++){
+		AS3_ByteArray_readBytes(raw_data, src, (Enc_Inst.blockl * sizeof(short)));
+		len = encode(&Enc_Inst, encoded_data, raw_data);
+		AS3_ByteArray_writeBytes(dest, encoded_data, len);
+		if(i % 10 == 0){
 			AS3_CallT(progress, NULL, "IntType", i);
 			flyield();//yield to main process
 		}
-    }
-    
-	AS3_Call(complete, NULL, NULL);
+	}
 }
 
 static void decodeForFlash(void * self, AS3_Val args)
 {
 	AS3_Val open, progress, complete;
 	AS3_Val src, dest;
-	int len;
+	int len, srcLen, yieldTicks;
 	short encoded_data[ILBCNOOFWORDS_MAX], decoded_data[BLOCKL_MAX];
-	
 
-	AS3_ArrayValue(args, "AS3ValType, AS3ValType, AS3ValType, AS3ValType, AS3ValType", &open, &progress, &complete, &src, &dest);
-	
-	AS3_Call(open, NULL, NULL);
+	AS3_ArrayValue(args, "AS3ValType, AS3ValType, AS3ValType, IntType, IntType", &progress, &src, &dest, &srcLen, &yieldTicks);
 
-	//AS3_CallT(progress, NULL, "IntType", 0);
 	iLBC_Dec_Inst_t Dec_Inst;
 	initDecode(&Dec_Inst, 30, 1);//30ms mode
-    
-    int i = 0;
-    while(AS3_ByteArray_readBytes(&encoded_data, src, Dec_Inst.no_of_bytes)){
+	
+	int i;
+	int loops = srcLen / Dec_Inst.no_of_bytes;
+	for(i = 0; i < loops; i++){
+		AS3_ByteArray_readBytes(encoded_data, src, Dec_Inst.no_of_bytes);
 		len=decode(&Dec_Inst, decoded_data, encoded_data, 1);//1 for no packet loss
-        AS3_ByteArray_writeBytes(dest, &decoded_data, len);
-        /* write output file */
-		if(i++ % 10 == 0){
-            AS3_CallT(progress, NULL, "IntType", i);
-            flyield();//yield to main process
+		AS3_ByteArray_writeBytes(dest, decoded_data, len);
+		/* write output file */
+		if(i % yieldTicks == 0){
+			AS3_CallT(progress, NULL, "IntType", i);
+			flyield();//yield to main process
 		}
-        
 	}
-    
-	AS3_Call(complete, NULL, NULL);
 }
 
 int main(int argc, char **argv)
 {
 	AS3_Val ilbc_lib = AS3_Object("");
-    AS3_Val encodeMethod = AS3_FunctionAsync( NULL, (AS3_ThunkProc) encodeForFlash);
+	AS3_Val encodeMethod = AS3_FunctionAsync( NULL, (AS3_ThunkProc) encodeForFlash);
 	AS3_Val decodeMethod = AS3_FunctionAsync( NULL, (AS3_ThunkProc) decodeForFlash);
-    AS3_SetS(ilbc_lib, "encode", encodeMethod);
-    AS3_SetS(ilbc_lib, "decode", decodeMethod);
+	AS3_SetS(ilbc_lib, "encode", encodeMethod);
+	AS3_SetS(ilbc_lib, "decode", decodeMethod);
 	AS3_Release( encodeMethod );
 	AS3_Release( decodeMethod );
-    
-    //No code below here. AS3_LibInit does not return
+	
+	//No code below here. AS3_LibInit does not return
 	AS3_LibInit( ilbc_lib );
 	return 0;
 }
